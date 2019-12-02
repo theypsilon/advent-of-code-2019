daabd996d2b41ec03088c3f38ea9efc7fc86cda4
José manuel Barroso Galindo <theypsilon@gmail.com>
2019-12-02 07:03:38 +0100

    Little optimization.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut program = PUZZLE_INPUT.to_vec();
     program[1] = 12;
     program[2] = 2;
-    let result = computer(&program)[0];
+    let result = computer(program)[0];
     println!("1. result: {}", result);
 
     let mut output: i64 = -1;
@@ -11,7 +11,7 @@ fn main() {
             let mut program = PUZZLE_INPUT.to_vec();
             program[1] = noun;
             program[2] = verb;
-            let result = computer(&program)[0];
+            let result = computer(program)[0];
             if result == 19690720 {
                 output = 100 * noun + verb;
                 break 'outer;
@@ -21,10 +21,8 @@ fn main() {
     println!("2. output: {}", output);
 }
 
-fn computer(input: &[i64]) -> Vec<i64> {
-    let mut result = input.to_vec();
+fn computer(mut result: Vec<i64>) -> Vec<i64> {
     let mut index = 0;
-
     loop {
         match result[index] {
             1 => {
@@ -58,16 +56,19 @@ mod test {
             .iter()
             .map(ToString::to_string)
             .collect::<Vec<String>>()
-            . join(",")
+            .join(",")
     }
 
     #[test]
     fn test_computer() {
-        assert_eq!(stringify(computer(&[1, 0, 0, 0, 99])), "2,0,0,0,99");
-        assert_eq!(stringify(computer(&[2, 3, 0, 3, 99])), "2,3,0,6,99");
-        assert_eq!(stringify(computer(&[2, 4, 4, 5, 99, 0])), "2,4,4,5,99,9801");
+        assert_eq!(stringify(computer(vec![1, 0, 0, 0, 99])), "2,0,0,0,99");
+        assert_eq!(stringify(computer(vec![2, 3, 0, 3, 99])), "2,3,0,6,99");
         assert_eq!(
-            stringify(computer(&[1, 1, 1, 4, 99, 5, 6, 0, 99])),
+            stringify(computer(vec![2, 4, 4, 5, 99, 0])),
+            "2,4,4,5,99,9801"
+        );
+        assert_eq!(
+            stringify(computer(vec![1, 1, 1, 4, 99, 5, 6, 0, 99])),
             "30,1,1,4,2,5,6,0,99"
         );
     }
