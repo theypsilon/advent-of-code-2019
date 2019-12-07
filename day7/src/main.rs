c3d62cb5044a32f5d5bc6be9a44fe1e113acb45c
José manuel Barroso Galindo <theypsilon@gmail.com>
2019-12-07 20:13:57 +0100

    Clarifying solution a bit.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 fn main() {
     let max_thruster = permutations(vec![0, 1, 2, 3, 4])
         .into_iter()
@@ -13,40 +15,38 @@ fn main() {
 }
 
 fn amplifiers_part1(instructions: Instructions, phase: PhaseSetting) -> i64 {
-    let mut output = vec![0];
+    let mut output = 0;
     for digit in phase {
-        let mut input = vec![digit];
-        input.extend(output);
-        let computation = Computer::with_input(instructions.clone(), input).run_until_halt();
-        output = computation.1;
+        output = Computer::new(instructions.clone())
+            .with_input(digit)
+            .with_input(output)
+            .run()
+            .outputs[0];
     }
-    output[0]
+    output
 }
 
 fn amplifiers_part2(instructions: Instructions, phase: PhaseSetting) -> i64 {
-    let mut computers: Vec<Computer> = (0..5)
-        .map(|_| Computer::new(instructions.clone()))
+    const LAST_COMPUTER: usize = 4;
+    let mut computers: Vec<Computer> = (0..=LAST_COMPUTER)
+        .map(|i| Computer::new(instructions.clone()).with_input(phase[i]))
         .collect();
 
     let mut result = -1;
     let mut output = 0;
-    let mut first_loop = true;
     'feedback: loop {
         for (i, computer) in computers.iter_mut().enumerate() {
-            computer.set_input(if first_loop {
-                vec![phase[i], output]
-            } else {
-                vec![output]
-            });
-            match computer.run_until_output() {
+            computer.add_input(output);
+            match computer.yield_output() {
                 ComputerState::Paused(value) => output = value,
-                ComputerState::Halted => if i == 4 {
-                    break 'feedback
-                },
+                ComputerState::Halted => {
+                    if i == LAST_COMPUTER {
+                        break 'feedback;
+                    }
+                }
             }
         }
         result = output;
-        first_loop = false;
     }
     result
 }
@@ -76,13 +76,12 @@ fn permutations(list: Vec<i64>) -> Vec<Vec<i64>> {
 
 type PhaseSetting = Vec<i64>;
 type Instructions = Vec<i64>;
-type ComputerInput = Vec<i64>;
-type ComputerOutput = Vec<i64>;
 
 struct Computer {
     instructions: Instructions,
-    input: ComputerInput,
+    input: VecDeque<i64>,
     index: usize,
+    finalized: bool,
 }
 
 enum ComputerState {
@@ -90,37 +89,48 @@ enum ComputerState {
     Halted,
 }
 
+struct ComputerResult {
+    #[cfg(test)]
+    instructions: Instructions,
+    outputs: Vec<i64>,
+}
+
 impl Computer {
     pub fn new(instructions: Instructions) -> Self {
         Computer {
             instructions,
-            input: vec![],
+            input: VecDeque::new(),
             index: 0,
+            finalized: false,
         }
     }
 
-    pub fn with_input(instructions: Instructions, input: ComputerInput) -> Self {
-        Computer {
-            instructions,
-            input,
-            index: 0,
-        }
+    pub fn with_input(mut self, input: i64) -> Self {
+        self.add_input(input);
+        self
     }
 
-    pub fn set_input(&mut self, input: ComputerInput) {
-        self.input = input;
+    pub fn add_input(&mut self, input: i64) {
+        self.input.push_back(input);
     }
 
-    pub fn run_until_halt(mut self) -> (Instructions, ComputerOutput) {
+    pub fn run(mut self) -> ComputerResult {
         let mut outputs = vec![];
-        while let ComputerState::Paused(value) = self.run_until_output() {
+        while let ComputerState::Paused(value) = self.yield_output() {
             outputs.push(value);
         }
-        (self.instructions, outputs)
+        ComputerResult {
+            #[cfg(test)]
+            instructions: self.instructions,
+            outputs,
+        }
     }
 
-    pub fn run_until_output(&mut self) -> ComputerState {
+    pub fn yield_output(&mut self) -> ComputerState {
         loop {
+            if self.finalized {
+                return ComputerState::Halted;
+            }
             let op = self.opcode();
             match op.de {
                 1 => {
@@ -136,8 +146,11 @@ impl Computer {
                     self.index += 4;
                 }
                 3 => {
-                    let input = self.input.remove(0);
-                    self.set(op.c, 1, input);
+                    if let Some(input) = self.input.pop_front() {
+                        self.set(op.c, 1, input);
+                    } else {
+                        panic!("Missing input!");
+                    }
                     self.index += 2;
                 }
                 4 => {
@@ -181,7 +194,7 @@ impl Computer {
                     self.set(op.a, 3, if first == second { 1 } else { 0 });
                     self.index += 4;
                 }
-                99 => return ComputerState::Halted,
+                99 => self.finalized = true,
                 _ => panic!("Something went wrong!"),
             }
         }
@@ -245,15 +258,15 @@ mod test {
             )*
         };
     }
-    fn computer_one(instructions: Instructions) -> (Instructions, ComputerOutput) {
+    fn computer_one(instructions: Instructions) -> ComputerResult {
         computer_n(instructions, 1)
     }
 
-    fn computer_n(instructions: Instructions, input: i64) -> (Instructions, ComputerOutput) {
-        Computer::with_input(instructions, vec![input]).run_until_halt()
+    fn computer_n(instructions: Instructions, input: i64) -> ComputerResult {
+        Computer::new(instructions).with_input(input).run()
     }
     fn get_value(instructions: Instructions, mode: i64, value: i64) -> i64 {
-        Computer::with_input(instructions, vec![0]).get(mode, value)
+        Computer::new(instructions).with_input(0).get(mode, value)
     }
 
     fn stringify(program: Instructions) -> String {
@@ -265,10 +278,10 @@ mod test {
     }
 
     eq_tests! {
-        computer_one_1: stringify(computer_one(vec![1, 0, 0, 0, 99]).0) => "2,0,0,0,99";
-        computer_one_2: stringify(computer_one(vec![2, 3, 0, 3, 99]).0) => "2,3,0,6,99";
-        computer_one_3: stringify(computer_one(vec![2, 4, 4, 5, 99, 0]).0) => "2,4,4,5,99,9801";
-        computer_one_4: stringify(computer_one(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]).0) => "30,1,1,4,2,5,6,0,99";
+        computer_one_1: stringify(computer_one(vec![1, 0, 0, 0, 99]).instructions) => "2,0,0,0,99";
+        computer_one_2: stringify(computer_one(vec![2, 3, 0, 3, 99]).instructions) => "2,3,0,6,99";
+        computer_one_3: stringify(computer_one(vec![2, 4, 4, 5, 99, 0]).instructions) => "2,4,4,5,99,9801";
+        computer_one_4: stringify(computer_one(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]).instructions) => "30,1,1,4,2,5,6,0,99";
 
         opcodes_1: to_opcode(1002) => Opcode {a: 0, b: 1, c: 0, de: 2};
 
@@ -282,20 +295,20 @@ mod test {
         get_value_12: get_value(vec![1, 2], 1, 1) => 2;
         get_value_13: get_value(vec![1, 2, 3], 1, 2) => 3;
 
-        computer_n_01: computer_n(vec![3,9,8,9,10,9,4,9,99,-1,8], 8).1[0] => 1;
-        computer_n_02: computer_n(vec![3,9,8,9,10,9,4,9,99,-1,8], 7).1[0] => 0;
+        computer_n_01: computer_n(vec![3,9,8,9,10,9,4,9,99,-1,8], 8).outputs[0] => 1;
+        computer_n_02: computer_n(vec![3,9,8,9,10,9,4,9,99,-1,8], 7).outputs[0] => 0;
 
-        thruster_example_1: amplifiers_part1(vec![3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0], vec![4,3,2,1,0]) => 43210;
-        thruster_example_2: amplifiers_part1(vec![3,23,3,24,1002,24,10,24,1002,23,-1,23,
+        amplifiers_part1_example_1: amplifiers_part1(vec![3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0], vec![4,3,2,1,0]) => 43210;
+        amplifiers_part1_example_2: amplifiers_part1(vec![3,23,3,24,1002,24,10,24,1002,23,-1,23,
             101,5,23,23,1,24,23,23,4,23,99,0,0], vec![0, 1, 2, 3, 4]) => 54321;
-        thruster_example_3: amplifiers_part1(vec![3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+        amplifiers_part1_example_3: amplifiers_part1(vec![3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
                 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0], vec![1,0,4,3,2]) => 65210;
 
         test_permutations: permutations(vec![0, 1, 2]) => vec![vec![0, 1, 2], vec![0, 2, 1], vec![1, 0, 2], vec![1, 2, 0], vec![2, 0, 1], vec![2, 1, 0]];
 
-        test_example_4: amplifiers_part2(vec![3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+        amplifiers_part2_example_4: amplifiers_part2(vec![3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
             27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], vec![9,8,7,6,5]) => 139629729;
-        test_example_5: amplifiers_part2(vec![3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+        amplifiers_part2_example_5: amplifiers_part2(vec![3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
                 -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
                 53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10], vec![9,7,8,5,6]) => 18216;
     }
