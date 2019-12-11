577129933e1d876e688fa7237a501f575164026b
José manuel Barroso Galindo <theypsilon@gmail.com>
2019-12-11 23:40:15 +0100

    Day 10, Part 2.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,8 +1,9 @@
-use std::collections::HashSet;
-
 fn main() {
-    let max = max_visibility(PUZZLE_INPUT);
-    println!("1. max: {}", max);
+    let (max, (x, y)) = max_visibility(PUZZLE_INPUT).unwrap();
+    println!("1. max visibility at: {}", max);
+
+    let position = guess_nth(PUZZLE_INPUT, x, y, 200).unwrap();
+    println!("2. the 200th destroyed asteroid would be: {:?}", position);
 }
 
 fn parse_asteroids(input: &str) -> Vec<Vec<i64>> {
@@ -20,58 +21,141 @@ fn parse_asteroids(input: &str) -> Vec<Vec<i64>> {
         .collect()
 }
 
-fn max_visibility(input: &str) -> i64 {
+fn max_visibility(input: &str) -> Option<(usize, (usize, usize))> {
     let asteroids = parse_asteroids(input);
-    let mut max = -1;
+    let mut result = None;
     for y in 0..asteroids.len() {
         for x in 0..asteroids[0].len() {
             if asteroids[y][x] == 0 {
                 continue;
             }
-            let visibility = get_visibility(&asteroids, x as i64, y as i64);
-            if visibility > max {
-                max = visibility;
+            let detections = sonar_around(&asteroids, x, y);
+            let max = if let Some((max, _)) = result { max } else { 0 };
+            if detections.len() > max {
+                result = Some((detections.len(), (x, y)));
             }
         }
     }
-    max
+    result
 }
 
-fn get_visibility(asteroids: &[Vec<i64>], i: i64, j: i64) -> i64 {
-    let mut set: HashSet<(i64, i64)> = HashSet::new();
-    for y in 0..asteroids.len() {
-        for x in 0..asteroids[0].len() {
-            let dx = i - x as i64;
-            let dy = j - y as i64;
-            if (dx == 0 && dy == 0) || asteroids[y][x] == 0 {
-                continue;
+const RAYCASTING_RESOLUTION: f64 = 0.04;
+
+const THETA_START: f64 = -180.0 * std::f64::consts::PI * (1.0 / 180.0);
+const THETA_END: f64 = THETA_START + 2.0 * std::f64::consts::PI;
+const THETA_STEP: f64 = RAYCASTING_RESOLUTION * std::f64::consts::PI * (1.0 / 180.0);
+
+fn sonar_around(asteroids: &[Vec<i64>], x: usize, y: usize) -> Vec<(usize, usize)> {
+    let mut theta = THETA_END;
+    let mut detections = vec![];
+    while theta > THETA_START {
+        let (tx, ty) = theta.sin_cos();
+        if let Some(col) = trace_ray(asteroids, x, y, MVec::new(tx, ty)) {
+            if !detections.contains(&col) {
+                detections.push(col);
             }
-            set.insert(to_i64(normalize((dx as f64, dy as f64))));
+        }
+        theta -= THETA_STEP;
+    }
+    detections
+}
+
+const RAY_STEP: f64 = RAYCASTING_RESOLUTION;
+const MIN_DISTANCE: f64 = RAYCASTING_RESOLUTION;
+
+fn trace_ray(
+    asteroids: &[Vec<i64>],
+    origin_x: usize,
+    origin_y: usize,
+    trajectory: MVec,
+) -> Option<(usize, usize)> {
+    let mut ray = from_usize_to_f64(origin_x, origin_y);
+    while ray.y < asteroids.len() as f64
+        && ray.x < asteroids[0].len() as f64
+        && ray.x > 0.0
+        && ray.y > 0.0
+    {
+        let (cell_x, cell_y) = from_f64_to_usize(ray);
+        ray = ray + trajectory * RAY_STEP;
+        if cell_x == origin_x && cell_y == origin_y || asteroids[cell_y][cell_x] != 1 {
+            continue;
+        }
+        let distance = module(ray - from_usize_to_f64(cell_x, cell_y));
+        if distance <= MIN_DISTANCE {
+            return Some((cell_x, cell_y));
         }
     }
-    set.len() as i64
+    None
 }
 
-const INT_FACTOR: f64 = 10_000.0;
-
-fn to_i64(n: (f64, f64)) -> (i64, i64) {
-    ((n.0 * INT_FACTOR) as i64, (n.1 * INT_FACTOR) as i64)
+fn guess_nth(input: &str, x: usize, y: usize, n: usize) -> Option<(usize, usize)> {
+    let mut asteroids = parse_asteroids(input);
+    let mut destroyed = 0;
+    loop {
+        let detections = sonar_around(&asteroids, x, y);
+        if detections.is_empty() {
+            break;
+        }
+        for (x, y) in detections {
+            asteroids[y][x] = 0;
+            destroyed += 1;
+            if destroyed == n {
+                return Some((x, y));
+            }
+        }
+    }
+    None
 }
 
-#[allow(dead_code)]
-fn to_f64(n: (i64, i64)) -> (f64, f64) {
-    ((n.0 as f64 / INT_FACTOR), (n.1 as f64 / INT_FACTOR))
+#[derive(Copy, Clone, Debug)]
+struct MVec {
+    x: f64,
+    y: f64,
 }
 
-fn module(n: (f64, f64)) -> f64 {
-    ((n.0 * n.0 + n.1 * n.1) as f64).sqrt()
+impl MVec {
+    pub fn new(x: f64, y: f64) -> Self {
+        MVec { x, y }
+    }
 }
 
-fn normalize(n: (f64, f64)) -> (f64, f64) {
-    let m = module(n);
-    let x = n.0 / m;
-    let y = n.1 / m;
-    (x, y)
+impl std::ops::Mul<f64> for MVec {
+    type Output = MVec;
+    fn mul(mut self, rhs: f64) -> Self::Output {
+        self.x *= rhs;
+        self.y *= rhs;
+        self
+    }
+}
+
+impl std::ops::Add for MVec {
+    type Output = MVec;
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self
+    }
+}
+
+impl std::ops::Sub for MVec {
+    type Output = MVec;
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self
+    }
+}
+
+fn from_usize_to_f64(x: usize, y: usize) -> MVec {
+    MVec::new(x as f64 + 0.5, y as f64 + 0.5)
+}
+
+fn from_f64_to_usize(n: MVec) -> (usize, usize) {
+    (n.x as usize, n.y as usize)
+}
+
+fn module(n: MVec) -> f64 {
+    ((n.x * n.x + n.y * n.y) as f64).sqrt()
 }
 
 #[cfg(test)]
@@ -89,15 +173,19 @@ mod test {
         };
     }
 
+    fn max_visibility_test(input: &str) -> usize {
+        max_visibility(input).unwrap().0
+    }
+
     eq_tests! {
         simple_parsing_1: parse_asteroids("#.\n##") => vec![vec![1,0],vec![1,1]];
         simple_parsing_2: parse_asteroids("..\n##") => vec![vec![0,0],vec![1,1]];
-        example_1: max_visibility(".#..#
+        example_1: max_visibility_test(".#..#
         .....
         #####
         ....#
         ...##") => 8;
-        example_2: max_visibility("......#.#.
+        example_2: max_visibility_test("......#.#.
         #..#.#....
         ..#######.
         .#.#.###..
@@ -107,7 +195,7 @@ mod test {
         .##.#..###
         ##...#..#.
         .#....####") => 33;
-        example_3: max_visibility("#.#...#.#.
+        example_3: max_visibility_test("#.#...#.#.
         .###....#.
         .#....#...
         ##.#.#.#.#
@@ -117,7 +205,7 @@ mod test {
         ..##....##
         ......#...
         .####.###.") => 35;
-        example_4: max_visibility(".#..#..###
+        example_4: max_visibility_test(".#..#..###
         ####.###.#
         ....###.#.
         ..###.##.#
@@ -127,7 +215,7 @@ mod test {
         #..#.#.###
         .##...##.#
         .....#.#..") => 41;
-        example_5: max_visibility(".#..##.###...#######
+        example_5: max_visibility_test(".#..##.###...#######
         ##.############..##.
         .#.######.########.#
         .###.#######.####.#.
@@ -147,6 +235,10 @@ mod test {
         .#.#.###########.###
         #.#.#.#####.####.###
         ###.##.####.##.#..##") => 210;
+        part_1: max_visibility_test(PUZZLE_INPUT) => 230;
+        sonar_around_clockwise: sonar_around(&parse_asteroids("###
+        ###
+        ###"), 1, 1).into_iter().collect::<Vec<_>>() => vec![(1, 0), (2, 0), (2, 1), (2, 2), (1, 2), (0, 2), (0, 1), (0, 0)];
     }
 }
 
